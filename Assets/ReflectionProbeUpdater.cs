08832c2f62e4916acaed9e94a305c88bfcb0b6a7
Daniel Lause <info@daniel-lause.de>
2016-02-24 17:49:10 +0100

    fixed bugs in EnemyAI

---
+++ Assets/ReflectionProbeUpdater.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ReflectionProbeUpdater : MonoBehaviour
+{
+    private Coroutine updater;
+    private ReflectionProbe reflectionProbe;
+
+    private void OnEnable()
+    {
+        reflectionProbe = GetComponent<ReflectionProbe>();
+        updater = StartCoroutine(ReflectionProbeUpdate());
+    }
+
+    private IEnumerator ReflectionProbeUpdate()
+    {
+        var waiter = new WaitForEndOfFrame();
+        var currentSlice = -1;
+        for (;;)
+        {
+            for (int i = 0; i < 2; i++)
+                yield return waiter;
+
+            currentSlice = reflectionProbe.RenderProbe();
+
+            while (!reflectionProbe.IsFinishedRendering(currentSlice))
+                yield return waiter;
+        }
+    }
+
+    private void OnDisable()
+    {
+        StopCoroutine(updater);
+    }
+}
