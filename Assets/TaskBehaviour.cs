053be7de40e259e618d3e17dd78e233156960060
Xellss <Xellsnr1@hotmail.de>
2016-02-24 21:50:53 +0100

    added tutorial

---
+++ Assets/TaskBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class TaskBehaviour : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject TaskTwo;
+    [SerializeField]
+    private GameObject TaskThree;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            TaskTwo.gameObject.SetActive(false);
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            TaskThree.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/TaskOne.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TaskOne : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject taskOne;
+
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "bullet")
+        {
+            this.gameObject.SetActive(false);
+            taskOne.SetActive(false);
+        }
+    }
+}
