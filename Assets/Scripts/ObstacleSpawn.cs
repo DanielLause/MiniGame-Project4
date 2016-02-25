047765b71d9c43a5828b2f7f1c8c4024c897149f
Daniel Lause <info@daniel-lause.de>
2016-02-25 13:32:24 +0100

    added obstacle script. : obstacales spawn random an 1 way always open

---
+++ Assets/Scripts/ObstacleSpawn.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class ObstacleSpawn : MonoBehaviour {
+
+    private Transform[] obstacals;
+
+	void Awake () {
+
+        obstacals = transform.Cast<Transform>().ToArray();
+        if (obstacals.Length > 1)
+                obstacals[Random.Range(0, obstacals.Length)].gameObject.SetActive(false);
+
+    }
+	
+	void Update ()
+    {
+	
+	}
+}
