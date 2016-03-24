db0062521e2e81326f039d6d19d522695dfdb4f0
Daniel Lause <info@daniel-lause.de>
2016-03-24 14:33:32 +0100

    added item effects, fixed enemy health

--- Assets/Scripts/ItemSpawn/TakeItem.cs
+++ Assets/Scripts/ItemSpawn/TakeItem.cs
@@ -7,14 +7,35 @@
 ///                                        ///
 //////////////////////////////////////////////
 
+using System.Collections;
 using UnityEngine;
 
 public class TakeItem : MonoBehaviour
 {
+    public string ItemName;
+
     [HideInInspector]
     public int ItemCount = 0;
 
-    public string ItemName;
+    [SerializeField, Tooltip("Healed the player. You can't take a \"OverHeal\".")]
+    private int mediPackHealth = 10;
+
+    [SerializeField, Tooltip("Gives the player more movespeed. Take a percent count"), Range(1, 100)]
+    private float drugPotSpeed = 20;
+
+    [SerializeField, Tooltip("Take a timer for DrugPot effect.")]
+    private float drugPotEffectTimer = 10;
+
+    private float oldMoveSpeed;
+    private float oldRunSpeed;
+    private bool drugEffectActive = false;
+
+    private void Start()
+
+    {
+        oldMoveSpeed = GetComponent<Movement>().MoveSpeed;
+        oldRunSpeed = GetComponent<Movement>().RunSpeed;
+    }
 
     public void ResetItemCount()
     {
@@ -26,9 +47,42 @@ public class TakeItem : MonoBehaviour
         ItemName = "";
     }
 
+    private IEnumerator drugPotTimer(float time)
+    {
+        yield return new WaitForSeconds(time);
+        GetComponent<Movement>().MoveSpeed = oldMoveSpeed;
+        GetComponent<Movement>().RunSpeed = oldRunSpeed;
+        drugEffectActive = false;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Item")
+        if (other.gameObject.tag == "Medipack")
+        {
+            if (GetComponent<Health>().CurrentHealth < GetComponent<Health>().MaxHealth)
+            {
+
+            ItemName = other.gameObject.name;
+            GetComponent<Health>().AddHealth(mediPackHealth);
+            GameObject.Destroy(other.gameObject);
+            ItemCount++;
+            }
+        }
+        if (other.gameObject.tag == "Drugpot")
+        {
+            if (!drugEffectActive)
+            {
+
+            ItemName = other.gameObject.name;
+            GetComponent<Movement>().MoveSpeed += (oldMoveSpeed / 100 * drugPotSpeed);
+            GetComponent<Movement>().RunSpeed += (oldRunSpeed / 100 * drugPotSpeed);
+            GameObject.Destroy(other.gameObject);
+                ItemCount++;
+                drugEffectActive = true;
+                StartCoroutine(drugPotTimer(drugPotEffectTimer));
+            }
+        }
+        if (other.gameObject.tag == "Pointer")
         {
             ItemName = other.gameObject.name;
             GameObject.Destroy(other.gameObject);
